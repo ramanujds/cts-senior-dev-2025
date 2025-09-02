e7ee90aaea3167f1ab5c89d7227bc3736e59a0a3
ramanujds <ramanujds9@gmail.com>
2025-09-02 15:03:19 +0530

    collections-case study

---
+++ java-projects/java-refresher/src/main/java/com/cts/Main.java
@@ -0,0 +1,15 @@
+package com.cts;
+
+import com.cts.model.Task;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+       Task task = Task.builder().title("abc").id(101).build();
+
+
+    }
+
+}
--- java-projects/java-refresher/src/main/java/com/cts/model/Task.java
+++ java-projects/java-refresher/src/main/java/com/cts/model/Task.java
@@ -1,7 +1,12 @@
 package com.cts.model;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.List;
 
+@Builder
+@Data
 public class Task {
 
     private int id;
@@ -10,61 +15,7 @@ public class Task {
     private String status;
     private List<String> users;
 
-    public Task(int id, String title, int priority, String status, List<String> users) {
-        this.id = id;
-        this.title = title;
-        this.priority = priority;
-        this.status = status;
-        this.users = users;
-    }
 
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", priority=" + priority +
-                ", status='" + status + '\'' +
-                '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public List<String> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<String> users) {
-        this.users = users;
-    }
 }
+
+
