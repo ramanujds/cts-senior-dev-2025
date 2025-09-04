e7b791145b0b4b7da6ed44e9e33f5e9ea2fa966e
ramanujds <ramanujds9@gmail.com>
2025-09-04 12:24:32 +0530

    Spring Boot profiles, swagger, spring jdbc

---
+++ spring-boot-projects/product-app-with-jdbc-template/src/main/java/com/cts/productapp/ProductAppWithJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.cts.productapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppWithJdbcTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductAppWithJdbcTemplateApplication.class, args);
+    }
+
+}
