4eebc0bbfea187e09c77771dca5009cd0f4dd9f4
ramanujds <ramanujds9@gmail.com>
2025-09-24 15:48:31 +0600

    react state management with reducer

--- react-js/my-stocks-app/src/features/stocks/StockList.jsx
+++ react-js/my-stocks-app/src/features/stocks/StockList.jsx
@@ -1,25 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { Row } from 'react-bootstrap'
 import StockItem from './StockItem'
-import { fetchStocks } from './stocksApi';
+import { deleteStockById, fetchStocks } from './stocksApi';
 
 const StockList = () => {
 
-    const [stocks, setStocks] = useState([]);
+    // const [stocks, setStocks] = useState([]);
+
+    const initialState = {
+        stocks: [],
+        loading: false,
+        error: null,
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    function reducer(state, action) {
+        switch (action.type) {
+            case 'LOADING':
+                return {
+                    ...state,
+                    loading: true,
+                    error: null,
+                };
+            case 'SUCCESS':
+                return {
+                    ...state,
+                    loading: false,
+                    stocks: action.payload,
+                };
+            case 'ERROR':
+                return {
+                    ...state,
+                    loading: false,
+                    error: action.payload,
+                };
+            case 'DELETE':
+                return {
+                    ...state,
+                    stocks: state.stocks.filter(stock => stock.id !== action.payload),
+                };
+            default:
+                return state;
+        }
+    }
+
 
     useEffect(() => {
-        fetchStocks().then(data => setStocks(data));
+        refreshStocks();
     }, []);
 
+    const refreshStocks = () => {
+        fetchStocks().then(data => {
+            dispatch({ type: 'SUCCESS', payload: data });
+        }).catch(error => {
+            dispatch({ type: 'ERROR', payload: error });
+            console.error('Error fetching stocks:', error);
+        });
+    }
+
     function deleteStock(id) {
-        setStocks(stocks.filter(stock => stock.id !== id));
-        console.log(`Deleted stock with id: ${id}`);
+      if(confirm("Are you sure you want to delete this stock?")){
+        deleteStockById(id).then(() => {
+            dispatch({ type: 'DELETE', payload: id });
+        })
+        .catch(error => {
+            dispatch({ type: 'ERROR', payload: error });
+            console.error('Error deleting stock:', error);
+        });
+      }
   }
 
   return (
     <>
     <Row>
-        {stocks.map(stock => (
+        {state.stocks.map(stock => (
             <StockItem key={stock.id} stock={stock} deleteStock={deleteStock}/>
         ))}
     </Row>
--- react-js/my-stocks-app/src/features/stocks/stocksApi.js
+++ react-js/my-stocks-app/src/features/stocks/stocksApi.js
@@ -7,3 +7,19 @@ export const fetchStocks = async () => {
         });
 };
 
+export const deleteStockById = async (id) => {
+    return fetch(`http://localhost:3000/stocks/${id}`, {
+        method: 'DELETE',
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.error('Error deleting stock:', error);
+        throw error;
+    });
+};
+
